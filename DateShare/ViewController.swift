88d14e10c5c92274345ee15eb45f75f4529f839c
nowperson <imajinkawabe@gmail.com>
2018-10-20 11:02:20 +0900

    Initial Commit

---
+++ DateShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DateShare
+//
+//  Created by Imajin Kawabe on 2018/10/20.
+//  Copyright © 2018年 Imajin Kawabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
